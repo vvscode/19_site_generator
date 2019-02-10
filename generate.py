24e724a459389de751bfdeea9e095e506a085972
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-02-10 20:54:43 +0400

    Single style

--- generate.py
+++ generate.py
@@ -7,25 +7,25 @@ import os
 import sys
 from collections import defaultdict
 
-INPUT_DIRECTORY = 'articles'
-OUTPUT_DIRECTORY = 'public'
-BASE_URL = 'https://vvscode.github.io/19_site_generator/public/'
+INPUT_DIRECTORY = "articles"
+OUTPUT_DIRECTORY = "public"
+BASE_URL = "https://vvscode.github.io/19_site_generator/public/"
 
 
 def get_articles_files():
-    return realsorted(glob.glob('{}/**/*.md'.format(INPUT_DIRECTORY)))
+    return realsorted(glob.glob("{}/**/*.md".format(INPUT_DIRECTORY)))
 
 
 def get_article_html(path, body):
-    return markdown2.markdown(body)
+    return markdown2.markdown(body, extras=["fenced-code-blocks"])
 
 
-def get_article_meta(path, body=''):
-    match = re.search(r'(\d+)_(.+?)/(\d*)_?(.+?)\.md', path)
+def get_article_meta(path, body=""):
+    match = re.search(r"(\d+)_(.+?)/(\d*)_?(.+?)\.md", path)
     section_id = int(match[1])
     title_id = int(match[3] or 0)
-    section = match[2].replace('_', ' ')
-    title = match[4].replace('_', ' ')
+    section = match[2].replace("_", " ")
+    title = match[4].replace("_", " ")
 
     section_info = (section, section_id)
     title_info = (title, title_id)
@@ -33,32 +33,32 @@ def get_article_meta(path, body=''):
     return section_info, title_info
 
 
-def get_page_html(title, content, base_url=BASE_URL, template_name='article'):
-    with open('templates/{}.html'.format(template_name), 'r') as template_file:
+def get_page_html(title, content, base_url=BASE_URL, template_name="article"):
+    with open("templates/{}.html".format(template_name), "r") as template_file:
         template = template_file.read()
-    return template.replace('{{TITLE}}', title).replace('{{CONTENT}}', content)
+    return template.replace("{{TITLE}}", title).replace("{{CONTENT}}", content)
 
 
 def get_article_relative_path(source_path):
-    relative_path = re.sub(r'^{}/'.format(INPUT_DIRECTORY), '', source_path)
-    relative_path = re.sub(r'\.md$', '.html', relative_path)
-    relative_path = re.sub(r'&.+;', '_', relative_path)
+    relative_path = re.sub(r"^{}/".format(INPUT_DIRECTORY), "", source_path)
+    relative_path = re.sub(r"\.md$", ".html", relative_path)
+    relative_path = re.sub(r"&.+;", "_", relative_path)
     return relative_path
 
 
 def put_file(path, content):
     os.makedirs(os.path.dirname(path), exist_ok=True)
-    with open(path, 'w') as target_file:
+    with open(path, "w") as target_file:
         target_file.write(content)
 
 
 def convert_articles(paths):
     for file_name in paths:
         target_file_name = file_name
-        target_file_name = '{}/{}'.format(OUTPUT_DIRECTORY,
+        target_file_name = "{}/{}".format(OUTPUT_DIRECTORY,
                                           get_article_relative_path(file_name))
 
-        with open(file_name, 'r') as source_file:
+        with open(file_name, "r") as source_file:
             content = source_file.read()
         html = get_article_html(file_name, content)
         _, title_info = get_article_meta(file_name)
@@ -71,23 +71,23 @@ def create_index(articles):
         section_info, title_info = get_article_meta(article_path)
         content[section_info].append((title_info, article_path))
 
-    html = '<ul class="ui list">'
+    html = "<ul class='ui list'>"
     for section_info in sorted(content.keys(), key=lambda x: x[0]):
-        html += '<li><span class="section-name">{}</span>'.format(
+        html += "<li><span class='section-name'>{}</span>".format(
             section_info[0])
-        html += '<ul class="ui list link">'
+        html += "<ul class='ui list link'>"
 
         for title_info, article_path in content[section_info]:
-            html += '<li class="article-name item"><a href="{}" class="item">{}</a></li>'.format(
+            html += "<li class='article-name item'><a href='{}' class='item'>{}</a></li>".format(
                 get_article_relative_path(article_path),
                 title_info[0])
 
-        html += '</ul>'
-        html += '</li>'
-    html += '</ul>'
+        html += "</ul>"
+        html += "</li>"
+    html += "</ul>"
 
-    put_file('public/index.html',
-             get_page_html('Main Page', html, template_name='index'))
+    put_file("public/index.html",
+             get_page_html("Main Page", html, template_name="index"))
 
 
 def make_site():
@@ -96,11 +96,11 @@ def make_site():
     create_index(articles)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     make_site()
 
-    if len(sys.argv) > 1 and sys.argv[1] == '--livereload':
+    if len(sys.argv) > 1 and sys.argv[1] == "--livereload":
         server = Server()
-        server.watch('{}/**/*.md'.format(INPUT_DIRECTORY), make_site)
-        server.watch('templates', make_site)
-        server.serve(root='public/')  # folder to serve html files from
+        server.watch("{}/**/*.md".format(INPUT_DIRECTORY), make_site)
+        server.watch("templates", make_site)
+        server.serve(root="public/")  # folder to serve html files from
